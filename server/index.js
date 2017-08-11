23cc03806c34aa6822dda8a744053ad57021fd74
Jeremiah Hunsicker <jeremiah.hunsicker@optum.com>
2017-08-11 12:09:29 -0400

    on lecture 23, refer to lecture 21 for keys

--- server/index.js
+++ server/index.js
@@ -1,10 +1,22 @@
 const express = require('express'); //have to use javascript common modules as nodejs doesn't support currently ES2015 modules
 //es2015 modules syntax: import express from 'express';
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Stategy;
+const keys = require('./config/keys');
+
 const app = express();   //generates a new app that represents a running express app
 
-app.get('/', (req, res)=>{
-  res.send({ bye: 'buddy'});
-});
+
+//client id 
+//client secret 
+passport.use(new GoogleStrategy({
+  clientID: keys.googleClientID,
+  clientSecret: keys.googleClientSecret,
+  callbackURL: '/auth/google/callback'
+  }, ()=>{
+    console.log(accessToken);
+  })
+);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
