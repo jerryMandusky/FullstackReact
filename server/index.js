d71e159361ea50bc3c423e1069f0e72dfc95ba87
sc.arletknight <sc.arletknight1807@gmail.com>
2017-08-10 23:19:26 -0400

    initial push - Lecture 12

---
+++ server/index.js
@@ -0,0 +1,9 @@
+const express = require('express'); //have to use javascript common modules as nodejs doesn't support currently ES2015 modules
+//es2015 modules syntax: import express from 'express';
+const app = express();   //generates a new app that represents a running express app
+
+app.get('/', (req, res)=>{
+  res.send({ hi: 'there'});
+});
+
+app.listen(5000);
