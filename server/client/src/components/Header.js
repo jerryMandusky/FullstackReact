a2d63783d6632b3ffda646a5effc21d152ab6fbe
sc.arletknight <sc.arletknight1807@gmail.com>
2017-08-15 22:51:41 -0400

    on lecture 96

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -1,31 +1,42 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
+import Payments from "./Payments";
 
 class Header extends Component {
-  renderContent(){
-    switch(this.props.auth){
+  renderContent() {
+    switch (this.props.auth) {
       case null:
         return;
       case false:
         return (
-          <li><a href="/auth/google">Login With Google</a></li>
+          <li>
+            <a href="/auth/google">Login With Google</a>
+          </li>
         );
-         default:
-        return <li><a href="/api/logout">Logout</a></li>;
+      default:
+        return [
+          <li key="1">
+            <Payments />
+          </li>,
+          <li key="2">
+            <a href="/api/logout">Logout</a>
+          </li>
+        ];
     }
   }
   render() {
     return (
       <nav>
         <div className="nav-wrapper">
-          <Link 
-          to={ this.props.auth ? "/surveys" : "/"  } 
-          className="left brand-logo">
-          Emaily
+          <Link
+            to={this.props.auth ? "/surveys" : "/"}
+            className="left brand-logo"
+          >
+            Emaily
           </Link>
           <ul className="right">
-           {this.renderContent()}
+            {this.renderContent()}
           </ul>
         </div>
       </nav>
@@ -33,8 +44,8 @@ class Header extends Component {
   }
 }
 
-function mapStateToProps({auth}}) {
-  return {auth};
+function mapStateToProps({ auth }) {
+  return { auth };
 }
 
 export default connect(mapStateToProps)(Header);
