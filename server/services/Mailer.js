fc27085009543519b5ef934831f8d3f3127213a6
Jeremiah Hunsicker <jeremiah.hunsicker@optum.com>
2017-08-17 16:17:53 -0400

    on Lecture 135

---
+++ server/services/Mailer.js
@@ -0,0 +1,55 @@
+const sendgrid = require("sendgrid");
+const helper = sendgrid.mail;
+const keys = require("../config/keys");
+
+class Mailer extends helper.Mail {
+  constructor({ subject, recipients }, content) {
+    //we're doing it this way because this is what sendGrid expects...
+    super();
+    this.sgApi = sendgrid(keys.sendGridKey);
+    this.from_email = new helper.Email("no-reply@emaily.com");
+    this.subject = subject;
+    this.body = new helper.Content("text/html", content);
+    this.recipients = this.formatAddresses(recipients);
+
+    this.addContent(this.body);
+    this.addClickTracking();
+    this.addRecipients();
+  }
+
+  formatAddress(recipients) {
+    return recipients.map(({ email }) => {
+      return new helper.Email(email);
+    });
+  }
+
+  //unfortunately this is how sendGrid works
+  addClickTracking() {
+    const trackingSettings = new helper.TrackingSettings();
+    const clickTracking = new helper.ClickTracking(true, true);
+
+    trackingSettings.setClickTracking(clickTracking);
+    this.addTrackingSettings(trackingSettings);
+  }
+
+  addRecipients() {
+    const personalize = new helper.Personalization();
+    this.recipients.forEach(recipient => {
+      personalize.addTo(recipient);
+    });
+    this.addPersonalization(personalize);
+  }
+
+  async send() {
+    const request = this.sgApi.emptyRequest({
+      method: "POST",
+      path: "/v3/mail/send",
+      body: this.toJSON()
+    });
+
+    this.sgApi.API(request);
+    return response;
+  }
+}
+
+module.exports = Mailer;
